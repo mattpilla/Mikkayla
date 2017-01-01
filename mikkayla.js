30edf3036872768e3892a4619c644d76f7717939
mattpilla <kaztalek@gmail.com>
2017-01-01 07:37:03 -0500

    wake and sleep

--- mikkayla.js
+++ mikkayla.js
@@ -12,51 +12,76 @@ var tech = JSON.parse(fs.readFileSync('json/tech.json', 'utf8'));
 var gun = 0;
 
 bot.on('message', (msg) => {
+    // Ignore bot shit
     if (msg.author.bot) {
         return;
     }
 
+    // Convenience function for sending messages
     function say(x) {
         msg.channel.sendMessage(x);
     }
 
+    // Get each word of command in an array
     let args = msg.content.split(' ');
-    if (msg.content === '.info') {
-        msg.reply('https://github.com/mattpilla/Mikkayla');
-    } else if (msg.content === 'hey') {
-        say('shut the fuck up');
-    } else if (args[0] === '.zfg') {
-        let index = +args[1] - 1;
-        if (!Number.isInteger(+index) || index < 0 || index >= zfgQuotes.length) {
-            index = randInt(zfgQuotes.length);
+
+    // Ignore everything if sleeping
+    if (bot.user.presence.status === 'dnd') {
+        if (args[0] === '.wake') {
+            if (!args[1] || args[1].includes(bot.user.id)) {
+                say('hiya :)');
+                bot.user.setStatus('online');
+            }
         }
-        let display = index + 1;
-        say('`' + display + ':` ' + zfgQuotes[index]);
-    } else if (args[0] == '.razor') {
-        let razorOpts = ['skip ', '', 'early '];
-        say('You might be able to get ' + read(items) + ' ' + read(razorOpts) + 'with ' + read(tech) + '.');
-    } else if (msg.content.toLowerCase().includes('mikkayla')) {
-        say(read(mikkaylaLines));
-    } else if (msg.content === '.roulette') {
-        if (!gun) {
-            gun = 6;
+    } else {
+        if (msg.content === '.info') {
+            msg.reply('https://github.com/mattpilla/Mikkayla');
+        } else if (msg.content === 'hey') {
+            say('shut the fuck up');
+        } else if (args[0] === '.zfg') {
+            let index = +args[1] - 1;
+            if (!Number.isInteger(+index) || index < 0 || index >= zfgQuotes.length) {
+                index = randInt(zfgQuotes.length);
+            }
+            let display = index + 1;
+            say('`' + display + ':` ' + zfgQuotes[index]);
+        } else if (args[0] == '.razor') {
+            let razorOpts = ['skip ', '', 'early '];
+            say('You might be able to get ' + read(items) + ' ' + read(razorOpts) + 'with ' + read(tech) + '.');
+        } else if (msg.content.toLowerCase().includes('mikkayla')) {
+            say(read(mikkaylaLines));
+        } else if (msg.content === '.roulette') {
+            if (!gun) {
+                gun = 6;
+            }
+            say(':grimacing: :gun: ' + gun + ' bullets in the chamber...');
+            if (randInt(gun) + 1 == gun) {
+                gun = 0;
+                say(msg.author + ' is dead :skull_crossbones:');
+            } else {
+                gun--;
+                say('u live.. for now :relieved:');
+            }
         }
-        say(':grimacing: :gun: ' + gun + ' bullets in the chamber...');
-        if (randInt(gun) + 1 == gun) {
-            gun = 0;
-            say(msg.author + ' is dead :skull_crossbones:');
-        } else {
-            gun--;
-            say('u live.. for now :relieved:');
+        /***
+         * Admin Shit
+         ***/
+        else if (msg.author.id === '85521124766539776' && msg.content.charAt(0) === '.') {
+            if (args[0] === '.sleep') {
+                if (!args[1] || args[1].includes(bot.user.id)) {
+                    say('later');
+                    bot.user.setStatus('dnd');
+                }
+            } else if (msg.content === '.update') {
+                say('give me a sec..');
+                require('child_process').exec('git pull');
+            } else if (msg.content === '.devastate') {
+                say('see ya');
+                setTimeout(function() {
+                    process.exit();
+                }, 1000);
+            }
         }
-    } else if (msg.content === '.update' && msg.author.id === '85521124766539776') {
-        say('give me a sec..');
-        require('child_process').exec('git pull');
-    } else if (msg.content === '.devastate' && msg.author.id === '85521124766539776') {
-        say('see ya');
-        setTimeout(function() {
-            process.exit();
-        }, 1000);
     }
 });
 
